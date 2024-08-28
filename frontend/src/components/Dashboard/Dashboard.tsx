7e9e71165685304b833c2af4f3dce8a331174210
Rutik <rutikkulkarni2001@gmail.com>
2024-08-28 19:44:40 +0530

    Refactor layout and styling for improved responsiveness and alignment

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -102,17 +102,18 @@ const Dashboard: React.FC = () => {
 
   return (
     <div className="p-6 pt-1">
-      <div className="flex flex-col lg:flex-row lg:items-center justify-between mb-6 mt-8 space-y-4 lg:space-y-0">
-        <div className="flex items-center justify-center">
+      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between mb-6 mt-8 space-y-4 lg:space-y-0">
+        <div className="flex items-center justify-center lg:justify-start w-full lg:w-auto">
           <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         </div>
+
         {notFound && (
-          <div className="text-red-500 text-center mt-4">
+          <div className="text-red-500 text-center lg:text-left mt-4 lg:mt-0">
             No results found for <b>{searchQuery}</b>
           </div>
         )}
 
-        <div className="flex flex-wrap items-center space-x-2 lg:space-x-4">
+        <div className="flex flex-wrap items-center justify-center lg:justify-end space-x-2 lg:space-x-4">
           <button className="flex items-center space-x-2 p-3 border border-gray-300 rounded-lg text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-500">
             <CiCalendar className="w-6 h-6" />
             <span className="text-sm font-medium">Calendar view</span>
