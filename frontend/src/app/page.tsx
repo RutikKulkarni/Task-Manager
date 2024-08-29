1762e288628cbd85af4a3f8b01a23413ea61aa62
Rutik <rutikkulkarni2001@gmail.com>
2024-08-29 13:03:37 +0530

    Added Loader UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,5 +14,14 @@ export default function MainPage() {
     }
   }, [router]);
 
-  return <p>Loading...</p>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+          <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
+        </div>
+        <p className="text-gray-700 text-lg font-semibold">Loading...</p>
+      </div>
+    </div>
+  );
 }
